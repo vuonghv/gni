293039c081e110d0011db28280e4c96665cc9f91
Hoang Vuong <hoang.van.vuong@framgia.com>
2015-09-09 17:16:26 +0700

    [ADD] Add views package

---
+++ gallery/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from gallery import views
+
+
+urlpatterns = [
+]
--- gni/urls.py
+++ gni/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^gni/', include('gallery.urls', namespace='gallery')),
     url(r'^admin/', include(admin.site.urls)),
 ]
