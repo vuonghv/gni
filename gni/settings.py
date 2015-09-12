4a0b318b211d878fc41ac7a242c237141876491c
Vuong Hoang <vuonghv.cs@gmail.com>
2015-09-13 00:46:06 +0700

    Changes to be committed: 	modified:   gni/settings.py [FINALLY] I Can Remove TEMPLATE_CONTEXT_PROCESSORS. wtf?

--- gni/settings.py
+++ gni/settings.py
@@ -70,15 +70,6 @@ TEMPLATES = [
 
 ]
 
-<<<<<<< HEAD
-=======
-"""
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
-    'gallery.contexts.appname',
-)
-"""
->>>>>>> 914543cbdfc88e614c883daaa93d53b583b93cbc
 
 WSGI_APPLICATION = 'gni.wsgi.application'
 
